47de422a5bedb2a490bb10dbb61bb3bc9dccb6de
Rafael Tosta <rafa.ecomp@gmail.com>
2024-05-23 08:03:29 -0300

    Update pos_install_ws.sh

--- pos_install_ws.sh
+++ pos_install_ws.sh
@@ -6,12 +6,8 @@
 ## Repositórios
 
 # Desativando repositórios não utilizados
-dnf config-manager --disable *-modular *-steam *-nvidia-driver *-PyCharm *-openh264
+dnf config-manager --disable *-steam *-nvidia-driver *-PyCharm *-openh264
  
-# RPM Fusion
-dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-
 # Zapzap
 dnf copr enable rafatosta/zapzap
 
@@ -19,25 +15,19 @@ dnf copr enable rafatosta/zapzap
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
-# Flatpak
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
-## Drivers Intel extends
-dnf -y install intel-media-driver libva-intel-driver libva-utils
-
 ## Removendo apps não utilizados
 dnf remove -y gnome-photos gnome-contacts gnome-tour gnome-boxes gnome-maps gnome-logs gnome-weather podman firefox abrt rhythmbox totem simple-scan mediawriter
 
 dnf autoremove
 
 ## Instalando programas pessoais
-dnf install -y megasync google-chrome-stable code vlc nodejs adw-gtk3-theme dialect evolution 
+dnf install -y google-chrome-stable code nodejs adw-gtk3-theme dialect zapzap 
 
 ## Instala as extensões
 dnf install -y gnome-shell-extension-user-theme gnome-shell-extension-appindicator
 
 ## Apps em flatpak
-flatpak install -y com.rtosta.zapzap com.mattjakeman.ExtensionManager \
+flatpak install -y com.mattjakeman.ExtensionManager \
 org.eclipse.Java com.github.tchx84.Flatseal org.telegram.desktop io.github.shiftey.Desktop \
 org.gtk.Gtk3theme.adw-gtk3 org.gtk.Gtk3theme.adw-gtk3-dark 
 
